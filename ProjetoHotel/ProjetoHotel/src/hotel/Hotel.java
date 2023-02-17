38451b75528d61aaa3d42ae0145e9232bcd58518
Roger Oliveira <41457384+Rjj18@users.noreply.github.com>
2023-02-17 11:18:30 -0300

    Add files via upload

--- ProjetoHotel/ProjetoHotel/src/hotel/Hotel.java
+++ ProjetoHotel/ProjetoHotel/src/hotel/Hotel.java
@@ -1,10 +1,56 @@
 package hotel;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Hotel {
 	
 	private int id;
 	private String nome;
 	private Endereco endereco;
+	private List<Quarto> quartos = new LinkedList<Quarto>();
 	
+	
+	public Hotel (int id, String nome, Endereco endereco) {
+		
+		this.nome = nome;
+		this.id = id;
+		this.endereco = endereco;
+		
+	}
 
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public List<Quarto> getQuartos() {
+		
+		return Collections.unmodifiableList(quartos);
+	}
+	
+	public void adiciona (Quarto quarto) {
+		this.quartos.add(quarto);
+	}
+
+	@Override
+	public String toString() {
+		
+		return "Id: " + id + "\nNome : " + nome + "\nEndereco" + endereco.toString();
+	}
 }
